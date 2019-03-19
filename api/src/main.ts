189f5d2c2782c39d582205f5c83665512446aba8
Christian Effiong <norexus007@gmail.com>
2019-03-19 11:04:48 +0100

    add environment variables

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,15 @@
+import { config } from 'dotenv';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+
+config();
+const port = process.env.PORT || 8080;
+const host = process.env.HOST || '127.0.0.1';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port);
+  Logger.log(`Server is running on http://${host}:${port}`, 'Bootstrap');
 }
 bootstrap();
