4afe47b00dd7b1c817c1a51bd4e2faa76995b4d8
Christian Effiong <norexus007@gmail.com>
2019-03-24 15:59:14 +0100

    include exception filter for error handling

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,14 +1,22 @@
 import { Module } from '@nestjs/common';
-import {TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { APP_FILTER } from '@nestjs/core';
 
 import configuration from './config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EntryModule } from './entry/entry.module';
+import { HttpExceptionFilter } from './shared/http-exception.filter';
 
 @Module({
   imports: [TypeOrmModule.forRoot(configuration.db), EntryModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+  ],
 })
 export class AppModule {}
