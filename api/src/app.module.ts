7a9a0e01ce62b68aa511be7953e362f052c5f543
Christian Effiong <norexus007@gmail.com>
2019-03-24 18:09:25 +0100

    add logging interceptor

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { APP_FILTER } from '@nestjs/core';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 
 import configuration from './config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EntryModule } from './entry/entry.module';
 import { HttpExceptionFilter } from './shared/http-exception.filter';
+import { LoggingInterceptor } from './shared/logging.interceptor';
 
 @Module({
   imports: [TypeOrmModule.forRoot(configuration.db), EntryModule],
@@ -16,6 +17,10 @@ import { HttpExceptionFilter } from './shared/http-exception.filter';
     {
       provide: APP_FILTER,
       useClass: HttpExceptionFilter,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggingInterceptor,
     },
   ],
 })
