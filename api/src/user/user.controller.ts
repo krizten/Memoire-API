71c87df521a555de63f1910fe7be1117e6981683
Christian Effiong <norexus007@gmail.com>
2019-03-28 16:42:41 +0100

    feat(user): add user login endpoint

--- api/src/user/user.controller.ts
+++ api/src/user/user.controller.ts
@@ -1,16 +1,36 @@
-import { Controller, Post, Body, UsePipes } from '@nestjs/common';
+import { Controller, Post, Body, UsePipes, Logger } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserDTO } from 'src/dto/user';
 import { ValidationPipe } from 'src/shared/validation.pipe';
+import { FileLogger } from 'src/shared/file-logger.service';
+import { LoginDTO } from 'src/dto/login';
 
 @Controller(`${process.env.BASE_PATH}/auth`)
 export class UserController {
 
   constructor(private userService: UserService) {}
 
+  private logger = new Logger('UserController');
+
   @Post('/signup')
   @UsePipes(new ValidationPipe())
   signup(@Body() data: UserDTO) {
+    FileLogger.log({
+      method: 'POST',
+      data,
+    });
+    this.logger.log(`${JSON.stringify({ method: 'POST', data })}`);
     return this.userService.signup(data);
+  }
+
+  @Post('/login')
+  @UsePipes(new ValidationPipe())
+  login(@Body() data: LoginDTO) {
+    FileLogger.log({
+      method: 'POST',
+      data,
+    });
+    this.logger.log(`${JSON.stringify({ method: 'POST', data })}`);
+    return this.userService.login(data);
   }
 }
--- api/src/user/user.service.ts
+++ api/src/user/user.service.ts
@@ -3,10 +3,12 @@ import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import * as jwt from 'jsonwebtoken';
+import * as bcrypt from 'bcryptjs';
 
 import { UserDTO } from 'src/dto/user';
 import { UserEntity } from './user.entity';
 import { IResponse } from 'src/interfaces/response';
+import { LoginDTO } from 'src/dto/login';
 
 config();
 
@@ -21,6 +23,11 @@ export class UserService {
   // get token
   private token(id: string, email: string): string {
     return jwt.sign({ id, email}, process.env.SECRET, { expiresIn: '7d' });
+  }
+
+  // confirm password
+  private async checkPassword(attempt: string, hash: string): Promise<boolean> {
+    return await bcrypt.compare(attempt, hash);
   }
 
   // format response to standard in specfications
@@ -44,7 +51,7 @@ export class UserService {
     };
   }
 
-  async signup(data: UserDTO) {
+  async signup(data: UserDTO): Promise<IResponse> {
     const email = data.email;
     let user = await this.userRepository.findOne({ where: { email }});
     if (user) {
@@ -53,5 +60,18 @@ export class UserService {
     user = await this.userRepository.create(data);
     await this.userRepository.save(user);
     return this.responseFormat('Signup successful', user, HttpStatus.CREATED, true);
+  }
+
+  async login(data: LoginDTO): Promise<IResponse> {
+    const { email, password } = data;
+    const user = await this.userRepository.findOne({ where: { email }});
+    if (!user) {
+      throw new HttpException('Email does not exist', HttpStatus.BAD_REQUEST);
+    }
+    const passwordMatch = await this.checkPassword(password, user.password);
+    if (!passwordMatch) {
+      throw new HttpException('Password is incorrect', HttpStatus.BAD_REQUEST);
+    }
+    return this.responseFormat('Login successful', user, HttpStatus.OK, true);
   }
 }
