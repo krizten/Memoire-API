2283b35f57149a38ad19ceca41263dbde8b06bc7
Christian Effiong <norexus007@gmail.com>
2019-04-15 20:36:22 +0100

    incorporate security plugins

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { config } from 'dotenv';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as helmet from 'helmet';
+import * as rateLimit from 'express-rate-limit';
+
+import { AppModule } from './app.module';
 import { FileLogger } from './shared/file-logger.service';
 
 config();
@@ -11,6 +14,22 @@ const host = process.env.HOST || '127.0.0.1';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Global Middlewares
+  app.use(helmet());
+  app.enableCors({
+    origin: '*',
+    allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'X-Access-Token', 'Authorization'],
+    credentials: true,
+    methods: ['GET', 'HEAD', 'OPTIONS', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    preflightContinue: false,
+  });
+  app.use(
+    new rateLimit({
+      windowMs: 15 * 60 * 1000,
+      max: 100,
+    }),
+  );
 
   const options = new DocumentBuilder()
     .setTitle('Memoire REST API')
