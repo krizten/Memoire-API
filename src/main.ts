47cacf56e7d9c5e42309309d3c8f284ee89ed4c9
Christian Effiong <norexus007@gmail.com>
2019-04-17 17:27:40 +0100

    chore(cleanup): update README

--- src/main.ts
+++ src/main.ts
@@ -32,11 +32,13 @@ async function bootstrap() {
   );
 
   const options = new DocumentBuilder()
-    .setTitle('Memoire REST API')
+    .setTitle('Memoire App WebAPI')
     .setDescription(
       'REST API endpoints documentation for Memoire app. Memoire is an online journal where users can pen down their thoughts and feelings.',
     )
     .setVersion('1.0')
+    .setContactEmail('mailstochristian@gmail.com')
+    .setLicense('MIT', 'https://github.com/krizten/Memoire-API/blob/dev/LICENSE')
     .addBearerAuth()
     .build();
 
